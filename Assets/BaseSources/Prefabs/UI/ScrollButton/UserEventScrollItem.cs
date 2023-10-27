b5a09ceedfd10ca049bb087f706caefbf77d0114
dhtmdgkr6 <dhtmdgkr6@google.com>
2023-10-27 12:19:58 +0900

    [Rabbit]기본 소스 커밋

---
+++ Assets/BaseSources/Prefabs/UI/ScrollButton/UserEventScrollItem.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using Seunghak.UIManager;
+
+public class UserEventScrollItem : OHScrollView.IInfiniteScrollSetup<UserEventScrollData>
+{
+    //이벤트 Window로 가는 게 있고, 해당 UI에서 바로 보여줄 게 잇고,
+    [SerializeField] private Button eventButton;
+    [SerializeField] private TextMeshProUGUI eventName;
+    [SerializeField] private Image eventButtonImage;
+    
+    public void OnPostSetupItems()
+    {
+        
+    }
+
+    public void OnUpdateItem( GameObject obj, UserEventScrollData infos)
+    {
+
+    }
+}
