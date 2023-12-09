f5d5014d25e5497de8f87199e9ca493b4c19ad5a
dhtmdgkr6 <dhtmdgkr6@google.com>
2023-12-10 02:13:25 +0900

    [오승학] 엑셀 컨버터 테스트용 코드

--- ExcelConverter/Script/JUnitData.cs
+++ ExcelConverter/Script/JUnitData.cs
@@ -3,14 +3,6 @@ public class JUnitData : JBaseData
 {
 	public int index;
 	public string unitName;
-	public int[] unitExp;
 	public int unitHp;
-	public int unitAttack;
-	public int unitDefence;
-	public int unitAttackRange;
-	public float unitCriticalPercent;
-	public int unitShield;
-	public int upgradeTableId;
-	public string unitPrefab;
-	public int aiType;
+	public int[] unitExp;
 }
